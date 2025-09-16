262c2275d4596c535c0c93298c7406dae0bf7b0f
Atefeh masoumi <a.masoumi@dorsa.cloud>
2025-09-16 13:51:20 +0330

    feat: enhance CustomerPage layout with Toolbar and CustomerManagementTable for improved customer management experience

--- src/pages/dashboards/customer/CustomerPage.tsx
+++ src/pages/dashboards/customer/CustomerPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Fragment } from 'react';
+import { Container } from '@/components/container';
+import { Toolbar, ToolbarHeading } from '@/layouts/demo1/toolbar';
+import { CustomerManagementTable } from '@/components/CustomerManagementTable';
 
 const CustomerPage = () => {
   return (
-    <div>CustomerPage</div>
-  )
-}
+    <Fragment>
+      <Container>
+        <Toolbar>
+          <ToolbarHeading title="مدیریت مشتریان" description="مشاهده، ویرایش و مدیریت اطلاعات مشتریان" />
+        </Toolbar>
+      </Container>
 
-export default CustomerPage
+      <Container>
+        <div className="flex flex-col items-stretch gap-5 lg:gap-7.5">
+          <CustomerManagementTable />
+        </div>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default CustomerPage;
